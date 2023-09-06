d4680abab82b31fcaeadce45bf6921f86aa6b2a9
Sudheer Pandit <sudheerpandit869@gmail.com>
2023-09-07 00:50:42 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,82 @@
+const addBtn= document.querySelector("#addBtn")
+
+const main= document.querySelector(".main")
+
+const saveNote= ()=>{
+    const notes= document.querySelectorAll(".note textarea");
+    const data= [];
+    notes.forEach(
+        (note)=>{
+            data.push(note.value)
+        }
+       
+    )
+
+if(data.length===0)
+{
+    localStorage.removeItem("notes")
+}
+else
+{
+    localStorage.setItem("notes",JSON.stringify(data))
+
+
+    }
+  
+
+addBtn.addEventListener("click",function(){
+   addNote()
+}
+)
+
+
+
+const addNote= (text="")=>{
+    const note= document.createElement("div");
+    note.classList.add("note")
+    note.innerHTML=`  
+    <div class="tool">
+    <i class=" save fas fa-save"></i>
+    <i class=" trash fas fa-trash"></i>
+   
+    
+    </div>
+<textarea > ${text}</textarea>
+
+`;
+
+note.querySelector(".trash").addEventListener("click",function(){
+    note.remove()
+});
+
+note.querySelector(".save").addEventListener("click",function(){
+    saveNote()
+})
+
+note.querySelector("textarea").addEventListener("focusout",function()
+{
+    saveNote();
+})
+main.appendChild(note);
+
+saveNote();
+}
+(
+    function(){
+        const lsnotes = JSON.parse(localStorage.getItem("notes"))
+       if(lsnotes===NUll)
+       {
+
+       }
+       else{
+        lsnotes.forEach(
+            (lsnotes)=>{
+                addNote(lsnotes);
+            }
+        )
+       }
+        
+       
+    }
+)()
+
